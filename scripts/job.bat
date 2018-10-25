201f4289ddc733ee9c795aa55e52aca1f0ada844
young-911 <727403273@qq.com>
2018-10-25 10:24:13 +0800

    update win start scripts/job.bat

--- scripts/job.bat
+++ scripts/job.bat
@@ -1,6 +1,6 @@
 @echo OFF
 
-cd H:\\develop\\dev-ubuntu14.4
+cd c:\\workplace\\lnmp-docker-compose\\scripts
 
 if "%1" == "start" goto start
 if "%1" == "stop" goto stop
@@ -8,11 +8,11 @@ goto done
 
 :start
 vagrant up
-vagrant ssh -c "sudo docker-compose -f /vagrant/docker-lnmp/docker-compose.yml up -d"
+vagrant ssh -c "sudo docker-compose -f /vagrant/docker-compose.yml up -d"
 goto done
 
 :stop
-vagrant ssh -c "sudo docker-compose -f /vagrant/docker-lnmp/docker-compose.yml stop"
+vagrant ssh -c "sudo docker-compose -f /vagrant/docker-compose.yml stop"
 vagrant suspend
 goto done
 
